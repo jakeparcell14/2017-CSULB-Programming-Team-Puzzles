e9dded8ebcb1658218e2aa72b9d63a1e3b42b09c
jakeparcell14 <jakeparcell14@gmail.com>
2018-07-15 23:14:40 -0700

    Retrieve user input until user is finished

--- Power Crisis/Power Crisis/src/PowerCrisis.java
+++ Power Crisis/Power Crisis/src/PowerCrisis.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * This program will take integers between 13 and 100 from the user. For each entry, the program will 
  * create a list of integers between 1 and the given integer. It will then find the lowest integer that,
@@ -9,11 +12,27 @@
  */
 public class PowerCrisis 
 {
-
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		
+		ArrayList<Integer> userInput = new ArrayList<Integer>();
+		
+		while(in.hasNext())
+		{
+			int entry = in.nextInt();
+			
+			if(entry != 0)
+			{
+				// add user input to list of entries
+				userInput.add(entry);
+			}
+			else
+			{
+				// stop retrieving user input
+				break;
+			}
+		}
 	}
 
 }
