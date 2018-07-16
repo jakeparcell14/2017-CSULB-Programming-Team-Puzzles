6102d2dd1a1d795a3e927b2667237a1460556ab3
jakeparcell14 <jakeparcell14@gmail.com>
2018-07-15 23:10:13 -0700

    Added program header

--- Power Crisis/Power Crisis/src/PowerCrisis.java
+++ Power Crisis/Power Crisis/src/PowerCrisis.java
@@ -1,5 +1,9 @@
 /**
- * 
+ * This program will take integers between 13 and 100 from the user. For each entry, the program will 
+ * create a list of integers between 1 and the given integer. It will then find the lowest integer that,
+ * when added to itself, will loop through the entire list of integers and reach the number 13 last. The
+ * integer being added to itself will continuously wrap around the list of integers to always create a 
+ * number between 1 and the given number that has not been landed on yet.
  * @author Jacob Parcell
  *
  */
