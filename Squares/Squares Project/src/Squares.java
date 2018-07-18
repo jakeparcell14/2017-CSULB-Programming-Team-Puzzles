71491e173edeee1f353217fd25982f167444e85e
jakeparcell14 <jakeparcell14@gmail.com>
2018-07-18 11:07:32 -0700

    Added end condition for user input loop

--- Squares/Squares Project/src/Squares.java
+++ Squares/Squares Project/src/Squares.java
@@ -8,32 +8,44 @@ public class Squares
 	{
 		Scanner in = new Scanner(System.in);
 		ArrayList<Integer> userInput = new ArrayList<Integer>();
-		
+
 		// take in user input
 		while(in.hasNextInt())
 		{
-			userInput.add(in.nextInt());
+			int entry = in.nextInt();
+
+			if(entry != 0)
+			{
+				userInput.add(entry);
+			}
+			else
+			{
+				break;
+			}
 		}
-				
+
+		System.out.println(Math.sqrt(9));
+
 		for(int i = 0; i < userInput.size(); i++)
 		{
 			// find results and print them to the console
 			System.out.println(findBase(userInput.get(i)));
 		}
 	}
-	
+
 	public static int findBase(int userInput)
 	{
 		// find the lowest possible numerical base
 		int lowestPossibleBase = findLargestNumber(userInput) + 1;
-		
-		
-		
-		
-		
-		return -1;
+
+		while
+
+
+
+
+			return -1;
 	}
-	
+
 	/**
 	 * This method changes a numbers numerical system
 	 * @param userInput		given integer to change	
@@ -44,17 +56,17 @@ public class Squares
 	{
 		String num = Integer.toString(userInput);
 		int result = 0;
-		
+
 		for(int i = 1; i <= num.length(); i++)
 		{
 			int multiplier = Integer.parseInt(num.substring(i - 1, i));
-			
+
 			result += ((int) Math.pow(base, num.length() - i)) * multiplier;
 		}
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * This program will go through each individual number of the integer and find the largest number
 	 * @param num	given integer
@@ -64,12 +76,12 @@ public class Squares
 	{
 		String s = Integer.toString(num);
 		int max = 0;
-		
+
 		// iterate through each number in the integer
 		for(int i = 0; i < s.length(); i++)
 		{
 			int possibleMax = Integer.parseInt(s.substring(i, i + 1));
-			
+
 			// compare number with max
 			if(possibleMax > max)
 			{
@@ -77,7 +89,7 @@ public class Squares
 				max = possibleMax;
 			}
 		}
-		
+
 		return max;		
 	}
 
