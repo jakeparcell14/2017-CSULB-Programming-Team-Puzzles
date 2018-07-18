9ec0ce6e0f2fbdd5e89a6c2c144880c3c2a14a40
jakeparcell14 <jakeparcell14@gmail.com>
2018-07-18 10:41:37 -0700

    Created findLargestNumber method
    This method will iterate through each number in the integer and find the largest number. This will save the program time by eliminating numerical systems that do not contain the largest number.

--- Squares/Squares Project/src/Squares.java
+++ Squares/Squares Project/src/Squares.java
@@ -15,10 +15,40 @@ public class Squares
 			userInput.add(in.nextInt());
 		}
 		
+		int[] results = new int[userInput.size()];
+		
 		for(int i = 0; i < userInput.size(); i++)
 		{
-			
+			results[i] = findBase(userInput.get(i));
 		}
+	}
+	
+	public static int findBase(int userInput)
+	{
+		
+		
+		
+		
+		
+		return -1;
+	}
+	
+	public static int findLargestNumber(int num)
+	{
+		String s = Integer.toString(num);
+		int max = 0;
+		
+		for(int i = 0; i < s.length(); i++)
+		{
+			int possibleMax = Integer.parseInt(s.substring(i, i + 1));
+			
+			if(possibleMax > max)
+			{
+				max = possibleMax;
+			}
+		}
+		
+		return max;		
 	}
 
 }
