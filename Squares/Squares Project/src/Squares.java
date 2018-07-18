4a18772a381e507cb0a2be8336503b8ce9545211
jakeparcell14 <jakeparcell14@gmail.com>
2018-07-18 10:46:46 -0700

    Refactored process of printing output
    Instead of saving my results to an array and then printing each one later, it is easier to just calculate the result and print it immediately.

--- Squares/Squares Project/src/Squares.java
+++ Squares/Squares Project/src/Squares.java
@@ -14,17 +14,18 @@ public class Squares
 		{
 			userInput.add(in.nextInt());
 		}
-		
-		int[] results = new int[userInput.size()];
-		
+				
 		for(int i = 0; i < userInput.size(); i++)
 		{
-			results[i] = findBase(userInput.get(i));
+			// find results and print them to the console
+			System.out.println(findBase(userInput.get(i)));
 		}
 	}
 	
 	public static int findBase(int userInput)
 	{
+		// find the lowest possible numerical base
+		int lowestPossibleBase = findLargestNumber(userInput) + 1;
 		
 		
 		
@@ -33,17 +34,25 @@ public class Squares
 		return -1;
 	}
 	
+	/**
+	 * This program will go through each individual number of the integer and find the largest number
+	 * @param num
+	 * @return
+	 */
 	public static int findLargestNumber(int num)
 	{
 		String s = Integer.toString(num);
 		int max = 0;
 		
+		// iterate through each number in the integer
 		for(int i = 0; i < s.length(); i++)
 		{
 			int possibleMax = Integer.parseInt(s.substring(i, i + 1));
 			
+			// compare number with max
 			if(possibleMax > max)
 			{
+				// overwrite max
 				max = possibleMax;
 			}
 		}
