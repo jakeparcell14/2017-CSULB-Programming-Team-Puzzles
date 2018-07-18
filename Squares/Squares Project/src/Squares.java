46555644cbc5ae55049a3be61e9257bc2f1549f7
jakeparcell14 <jakeparcell14@gmail.com>
2018-07-18 10:32:11 -0700

    Take in and store user input

--- Squares/Squares Project/src/Squares.java
+++ Squares/Squares Project/src/Squares.java
@@ -1,9 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Squares {
+public class Squares 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		ArrayList<Integer> userInput = new ArrayList<Integer>();
+		
+		// take in user input
+		while(in.hasNextInt())
+		{
+			userInput.add(in.nextInt());
+		}
+		
+		for(int i = 0; i < userInput.size(); i++)
+		{
+			
+		}
 	}
 
 }
