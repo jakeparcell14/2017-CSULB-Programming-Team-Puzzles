14fad1c1526af6de2f636dfc63494bd9b4fdcb74
jakeparcell14 <jakeparcell14@gmail.com>
2018-07-21 22:52:37 -0700

    Added divide method and user input handler

--- Modulus and Division Calculator/Mod Div Calculator/src/ModDivCalculator.java
+++ Modulus and Division Calculator/Mod Div Calculator/src/ModDivCalculator.java
@@ -1,9 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ModDivCalculator {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);		
+		ArrayList<Integer> results = new ArrayList<Integer>();
+		
+		while(in.hasNextLine())
+		{
+			String input = in.nextLine();
+			
+			String[] line = getLineInfo(input);
+			
+			if(results.size() > 3)
+			{
+				break;
+			}
+		}
+		
+	}
+	
+	public static String[] getLineInfo(String input)
+	{
+		return input.split(" ");
+	}
+	
+	public static int divide(int divisor, int dividend, int counter)
+	{
+		if(divisor < dividend)
+		{
+			return counter;
+		}
+		else
+		{
+			divide(divisor - dividend, dividend, counter++);
+		}
+		
+		return 0;
 	}
 
 }
