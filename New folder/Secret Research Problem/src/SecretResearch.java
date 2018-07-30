bd4c601d3cdb9df90432379748f4c0077ce2efa4
jakeparcell14 <jakeparcell14@gmail.com>
2018-07-29 22:25:19 -0700

    Process user input

--- New folder/Secret Research Problem/src/SecretResearch.java
+++ New folder/Secret Research Problem/src/SecretResearch.java
@@ -1,10 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SecretResearch 
 {
 
 	public static void main(String[] args) 
 	{
+		Scanner in = new Scanner(System.in);
 		
+		ArrayList<String> results = new ArrayList<String>();
+		
+		while(in.hasNextInt())
+		{
+			
+		}
 
 	}
 
