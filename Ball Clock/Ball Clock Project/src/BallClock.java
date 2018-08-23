cb3204ff32e8c1949f25b7704940a4f0f7a53566
jakeparcell14 <jakeparcell14@gmail.com>
2018-08-22 23:40:53 -0700

    Added tilt conditions and clock incrementer

--- Ball Clock/Ball Clock Project/src/BallClock.java
+++ Ball Clock/Ball Clock Project/src/BallClock.java
@@ -11,7 +11,7 @@ public class BallClock
 	{
 		Scanner in = new Scanner(System.in);
 
-		ArrayList<Integer> results = new ArrayList<Integer>();
+		ArrayList<String> results = new ArrayList<String>();
 
 		while(in.hasNextInt())
 		{
@@ -19,11 +19,8 @@ public class BallClock
 
 			if(input != 0)
 			{
-				//solve
-				int result = simulateClock(input);
-
-				// print result
-				System.out.println(input + " balls cycle after " + result + " days.");
+				// save result
+				results.add(input + " balls cycle after " + simulateClock(input) + " days.");
 			}
 			else
 			{
@@ -32,10 +29,8 @@ public class BallClock
 		}
 	}
 
-	public static int simulateClock(int numberOfBalls)// TODO do i need the number of balls in this method?
+	public static int simulateClock(int numberOfBalls)
 	{
-		Queue<Integer> waitingBalls = new LinkedList<Integer>();
-
 		// can hold at most 4 balls
 		Stack<Integer> minutes = new Stack<Integer>();
 
@@ -45,29 +40,59 @@ public class BallClock
 		// can hold at most 11 balls
 		Stack<Integer> hours = new Stack<Integer>();
 
-		return 0;
+		// holds all balls that are not currently in a track
+		Queue<Integer> waitingBalls = new LinkedList<Integer>();
+
+		// set waiting balls in initial position
+		fillInitialWaitingBalls(waitingBalls, hours, numberOfBalls);
+
+		double halfDayCount = 0;
+
+		do
+		{
+			//increment clock
+			minutes.push(waitingBalls.remove());
+
+			if(minutes.size() > 4)
+			{
+				tilt(minutes, fiveMinutes, waitingBalls, false);
+			}
+
+			if(fiveMinutes.size() > 11)
+			{
+				tilt(fiveMinutes, hours, waitingBalls, false);
+
+			}
+
+			if(hours.size() > 11)
+			{
+				tilt(hours, null, waitingBalls, true);
+				halfDayCount++;
+			}
+		}
+		while(waitingBalls.peek() != 1);
+
+		return (int) halfDayCount / 2;
 	}
 
 	public static void fillInitialWaitingBalls(Queue<Integer> waitingBalls, Stack<Integer> hours, int numberOfBalls)
 	{
 		// add fixed ball in hours track
 		hours.push(0);
-		
+
 		for(int i = 1; i <= numberOfBalls; i++)
 		{
 			waitingBalls.add(i);
 		}
-		
-		
 	}
 
-	public static void tilt(Stack<Integer> track, Stack<Integer> nextTrack, Queue<Integer> waiting, boolean hoursTrack)
+	public static int tilt(Stack<Integer> track, Stack<Integer> nextTrack, Queue<Integer> waiting, boolean hoursTrack)
 	{
 		// hold ball that caused the track to tilt
 		int tilterBall = track.pop();
-		
+
 		// empty lower balls back into waiting queue
-		while(track.size() >= 1) 
+		while(track.size() > 1) 
 		{
 			waiting.add(track.pop());
 		}
@@ -76,14 +101,20 @@ public class BallClock
 		{
 			// empty last ball in track
 			waiting.add(track.pop());
-			
+
 			// send tilter ball to next stack
 			nextTrack.push(tilterBall);
+
+			// do not increment half day count
+			return 0;
 		}
 		else
 		{
 			// leave last ball in the hours track because it's fixed and send tilter ball to waiting queue
 			waiting.add(tilterBall);
+
+			// increment half day count
+			return 1;
 		}
 
 	}
