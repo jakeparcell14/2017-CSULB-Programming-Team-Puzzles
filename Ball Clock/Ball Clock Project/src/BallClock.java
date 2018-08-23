dbccee9199fda70d6660845b54ffcfa56335f5da
jakeparcell14 <jakeparcell14@gmail.com>
2018-08-22 22:40:55 -0700

    Created Outline of the Program

--- Ball Clock/Ball Clock Project/src/BallClock.java
+++ Ball Clock/Ball Clock Project/src/BallClock.java
@@ -1,8 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class BallClock {
+public class BallClock 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		
+		ArrayList<Integer> results = new ArrayList<Integer>();
+		
+		while(in.hasNextInt())
+		{
+			int input = in.nextInt();
+			
+			if(input != 0)
+			{
+				//solve
+			}
+			else
+			{
+				break;
+			}
+		}
+		
+		//print results
 
 	}
 
